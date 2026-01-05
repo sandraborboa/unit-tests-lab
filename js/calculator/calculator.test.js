8663a0f61fc2a020649500aa421a7c55f36265a4
Sandra Borboa <sborboaluna@gmail.com>
2026-01-05 15:49:27 -0700

    Primer commit

---
+++ js/calculator/calculator.test.js
@@ -0,0 +1,29 @@
+const calculator = require('./calculator');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+test('adds -4 + 8 to equal 4', () => {
+  expect(calculator.add(-4, 8)).toBe(4);
+});
+
+test('subtract 5 - 4 to equal 1', () => {
+  expect(calculator.subtract(5, 4)).toBe(1);
+});
+
+test('subtract -5 - -10 to equal 5', () => {
+  expect(calculator.subtract(-5, -10)).toBe(5);
+});
+
+// 2 pruebas unitarias para la función divide()
+
+test("divides 20 / 5 to equal 4", () => {
+  expect(calculator.divide(20, 5)).toBe(4);
+});
+
+test("divides 80 / 2 to equal 40", () => {
+  expect(calculator.divide(80, 2)).toBe(40);
+});
+
+// 2 pruebas unitarias para la función multiply().
